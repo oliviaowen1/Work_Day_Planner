b9004e80d6599ca20989b35a4f8d16b22506c935
Olivia Owen <olivia.owen@hotmail.com>
2021-01-15 20:48:25 +0000

    Added the current day, date and time

---
+++ Develop/script.js
@@ -0,0 +1,3 @@
+// // Below will display the current date and day for the day you use the webpage
+// This uses moment.js to give the date, everything is the brackets can be amended to whatever date format you choose.
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
