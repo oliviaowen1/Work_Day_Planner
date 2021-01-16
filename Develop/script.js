4b49ddd7711cce1c1eb50789bb0298589018f5e6
Olivia Owen <olivia.owen@hotmail.com>
2021-01-16 23:51:51 +0000

    Added function to save input to local storage and retrieve input

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,17 @@
+// This function means that these functions run on the loading or opening of the window.
+
 window.onload = function () {
     changeColour();
+    saveNine();
+    saveTen();
+    saveEleven();
+    saveTwelve();
+    saveThirteen();
+    saveFourteen();
+    saveFifteen();
+    saveSixteen();
+    saveSeventeen();
+   }
 }
 // // Below will display the current date and day for the day you use the webpage
 // This uses moment.js to give the date, everything is the brackets can be amended to whatever date format you choose.
@@ -33,6 +45,7 @@ function changeColour() {
     else if (currentTime < 10) {
         $("#10AM").addClass("future");
     }
+    // 11 AM
     if (currentTime > 11) {
         $("#11AM").addClass("past");
     }
@@ -42,6 +55,7 @@ function changeColour() {
     else if (currentTime < 11) {
         $("#11AM").addClass("future");
     }
+    // 12 PM
     if (currentTime > 12) {
         $("#12PM").addClass("past");
     }
@@ -51,6 +65,7 @@ function changeColour() {
     else if (currentTime < 12) {
         $("#12PM").addClass("future");
     }
+    // 13 PM
     if (currentTime > 13) {
         $("#13PM").addClass("past");
     }
@@ -60,6 +75,7 @@ function changeColour() {
     else if (currentTime < 13) {
         $("#13PM").addClass("future");
     }
+    // 14PM
     if (currentTime > 14) {
         $("#14PM").addClass("past");
     }
@@ -69,6 +85,7 @@ function changeColour() {
     else if (currentTime < 14) {
         $("#14PM").addClass("future");
     }
+    // 15 PM
     if (currentTime > 15) {
         $("#15PM").addClass("past");
     }
@@ -78,6 +95,7 @@ function changeColour() {
     else if (currentTime < 15) {
         $("#15PM").addClass("future");
     }
+    // 16 PM
     if (currentTime > 16) {
         $("#16PM").addClass("past");
     }
@@ -87,6 +105,7 @@ function changeColour() {
     else if (currentTime < 16) {
         $("#16PM").addClass("future");
     }
+    // 17 PM
     if (currentTime > 17) {
         $("#17PM").addClass("past");
     }
@@ -97,3 +116,170 @@ function changeColour() {
         $("#17PM").addClass("future");
     }
 }
+//  The function below will save the input in local storage and keep it there even if the page is refreshed or closed and reopened
+// This function is run when the page opens to ensure that data loads as soon as the page is refreshed or opened.
+function saveNine() {
+    // the below takes the ID for the input field and holds them in variables
+    // please note the input and output are saved as the same variable as this will hold the same information and the information will
+    // be put back into the same input field.
+    var input_9 = document.querySelector("#Text9");
+    var output_9 = document.querySelector("#Text9");
+    var save_button9 = document.querySelector("#saveBtn9");
+
+//   the below waits for the click and runs the function to update the input field
+    save_button9.addEventListener("click", updateOutput9);
+
+//   The below holds the text content and values of the input field and saves this in the local storage
+    output_9.textContent = localStorage.getItem("content");
+    input_9.value = localStorage.getItem("content");
+  
+// this function is used to return the value of the input field, that has been stored in the local storage, back to the input field.
+    function updateOutput9() {
+      localStorage.setItem("content", input_9.value);
+  
+      output_9.textContent = input_9.value;
+    }
+  }
+// Please note the above function has been run with each time block below:
+function saveTen() {
+    var input_10 = document.querySelector("#Text10");
+    var output_10 = document.querySelector("#Text10");
+    var save_button10 = document.querySelector("#saveBtn10");
+  
+    save_button10.addEventListener("click", updateOutput10);
+  
+    output_10.textContent = localStorage.getItem("content10");
+    input_10.value = localStorage.getItem("content10");
+  
+    function updateOutput10() {
+      localStorage.setItem("content10", input_10.value);
+  
+      output_10.textContent = input_10.value;
+    }
+  }
+  
+  
+  function saveEleven() {
+    var input_11 = document.querySelector("#Text11");
+    var output_11 = document.querySelector("#Text11");
+    var save_button11 = document.querySelector("#saveBtn11");
+  
+    save_button11.addEventListener("click", updateOutput11);
+  
+    output_11.textContent = localStorage.getItem("content11");
+    input_11.value = localStorage.getItem("content11");
+  
+    function updateOutput11() {
+      localStorage.setItem("content11", input_11.value);
+  
+      output_11.textContent = input_11.value;
+    }
+  }
+  
+  
+  function saveTwelve() {
+    var input_12 = document.querySelector("#Text12");
+    var output_12 = document.querySelector("#Text12");
+    var save_button12 = document.querySelector("#saveBtn12");
+  
+    save_button12.addEventListener("click", updateOutput12);
+  
+    output_12.textContent = localStorage.getItem("content12");
+    input_12.value = localStorage.getItem("content12");
+  
+    function updateOutput12() {
+      localStorage.setItem("content12", input_12.value);
+  
+      output_12.textContent = input_12.value;
+    }
+  }
+  
+  
+  function saveThirteen() {
+    var input_13 = document.querySelector("#Text13");
+    var output_13 = document.querySelector("#Text13");
+    var save_button13 = document.querySelector("#saveBtn13");
+  
+    save_button13.addEventListener("click", updateOutput13);
+  
+    output_13.textContent = localStorage.getItem("content13");
+    input_13.value = localStorage.getItem("content13");
+  
+    function updateOutput13() {
+      localStorage.setItem("content13", input_13.value);
+  
+      output_13.textContent = input_13.value;
+    }
+  }
+  
+  
+  function saveFourteen() {
+    var input_14 = document.querySelector("#Text14");
+    var output_14 = document.querySelector("#Text14");
+    var save_button14 = document.querySelector("#saveBtn14");
+  
+    save_button14.addEventListener("click", updateOutput14);
+  
+    output_14.textContent = localStorage.getItem("content14");
+    input_14.value = localStorage.getItem("content14");
+  
+    function updateOutput14() {
+      localStorage.setItem("content14", input_14.value);
+  
+      output_14.textContent = input_14.value;
+    }
+  }
+  
+  
+  function saveFifteen() {
+    var input_15 = document.querySelector("#Text15");
+    var output_15 = document.querySelector("#Text15");
+    var save_button15 = document.querySelector("#saveBtn15");
+  
+    save_button15.addEventListener("click", updateOutput15);
+  
+    output_15.textContent = localStorage.getItem("content15");
+    input_15.value = localStorage.getItem("content15");
+  
+    function updateOutput15() {
+      localStorage.setItem("content15", input_15.value);
+  
+      output_15.textContent = input_15.value;
+    }
+  }
+  
+  
+  function saveSixteen() {
+    var input_16 = document.querySelector("#Text16");
+    var output_16 = document.querySelector("#Text16");
+    var save_button16 = document.querySelector("#saveBtn16");
+  
+    save_button16.addEventListener("click", updateOutput16);
+  
+    output_16.textContent = localStorage.getItem("content16");
+    input_16.value = localStorage.getItem("content16");
+  
+    function updateOutput16() {
+      localStorage.setItem("content16", input_16.value);
+  
+      output_16.textContent = input_16.value;
+    }
+  }
+  
+  
+  function saveSeventeen() {
+    var input_17 = document.querySelector("#Text17");
+    var output_17 = document.querySelector("#Text17");
+    var save_button17 = document.querySelector("#saveBtn17");
+  
+    save_button17.addEventListener("click", updateOutput17);
+  
+    output_17.textContent = localStorage.getItem("content17");
+    input_17.value = localStorage.getItem("content17");
+  
+    function updateOutput17() {
+      localStorage.setItem("content17", input_17.value);
+  
+      output_17.textContent = input_17.value;
+    }
+  }
